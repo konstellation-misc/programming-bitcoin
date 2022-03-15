370dbcb11d3c224f57e6671e8da85e52547ef4b0
Beomsoo Kim <bluewhale8202@gmail.com>
2022-03-16 04:00:37 +0900

    Beoms ch06

---
+++ bitcoin/ch06/beoms/ex02.py
@@ -0,0 +1,17 @@
+from script import Script
+
+z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+sec_pub_key = bytes.fromhex(
+    '04887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e'
+    '4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34'
+)
+sig = bytes.fromhex(
+    '3045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f4048'
+    '76325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fd'
+    'dbdce6feab601')
+
+stack = [sig, sec_pub_key]
+
+from op import op_checksig
+
+op_checksig(stack, z)
