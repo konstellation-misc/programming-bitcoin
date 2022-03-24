8d8b6ff7478cb8c9400f54f57854cea90cb96182
leedc0101 <leedc0101@naver.com>
2022-03-24 11:59:47 +0900

    Init chapter 3

---
+++ bitcoin/ch03/dongchang/main.ts
@@ -0,0 +1,15 @@
+import { FieldElement } from '../../ch01/dongchang/field'
+import { Point } from '../../ch02/dongchang/point'
+
+function ex01() {
+
+}
+
+function main(){
+  try{
+    ex01()
+  }catch(e){
+    console.log(e)
+  }
+}
+
