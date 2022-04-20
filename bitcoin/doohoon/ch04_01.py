c9c5b1638d516741184c552373a8830d3974ffc5
Doohoo Kim <invi.dh.kim@gmail.com>
2022-04-20 10:03:25 +0900

    Add source of chapter 04-ex1 for prog. bitcoin.

--- bitcoin/doohoon/ch04_01.py
+++ bitcoin/doohoon/ch04_01.py
@@ -3,3 +3,11 @@ from ecc import PrivateKey
 private_key = PrivateKey(5000)
 
 print(private_key.point.sec(compressed=False).hex())
+
+private_key = PrivateKey(2018**5)
+
+print(private_key.point.sec(compressed=False).hex())
+
+private_key = PrivateKey(0xdeadbeef12345)
+
+print(private_key.point.sec(compressed=False).hex())
