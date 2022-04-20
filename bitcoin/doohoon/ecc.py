4c6822995f57a5b5002b6118d81ecd92815bf4ac
Doohoo Kim <invi.dh.kim@gmail.com>
2022-04-20 10:00:37 +0900

    Move some functions at ecc to helper.

--- bitcoin/doohoon/ecc.py
+++ bitcoin/doohoon/ecc.py
@@ -356,37 +356,3 @@ class PrivateKey:
             suffix = b''
 
         return encode_base58_checksum(prefix + secret_bytes + suffix)
-
-
-BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-
-
-def encode_base58(s):
-    count = 0
-
-    for c in s:
-        if c == 0:
-            count += 1
-        else:
-            break
-    
-    num = int.from_bytes(s, "big")
-    prefix = '1' * count
-    result = ''
-
-    while num > 0:
-        num, mod = divmod(num, 58)
-        result = BASE58_ALPHABET[mod]
-
-    return prefix + result
-
-
-def encode_base58_checksum(b):
-    return encode_base58(b + hash256(b)[:4])
-
-
-def hash160(s):
-    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
-
-def hash256():
-    return hashlib.new(hashlib.sha256(s).digest()).digest()
