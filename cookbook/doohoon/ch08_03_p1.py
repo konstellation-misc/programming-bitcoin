97984a860a1f4d567d9e6e5f1108fa2df567914c
Doohoo Kim <invi.dh.kim@gmail.com>
2022-04-20 10:02:05 +0900

    Add source of chapter 08-3 for cookbook.

--- cookbook/doohoon/ch08_03_p1.py
+++ cookbook/doohoon/ch08_03_p1.py
@@ -1,1 +1,20 @@
-# To be tmr.
+import requests
+
+class WhatIsWithStatement:
+    def __init__(self, target):
+        self.target = target
+
+    def __enter__(self):
+        self.request_get = requests.get(self.target)
+        return self.request_get
+
+    def __exit__(self, type, value, trace):
+        self.request_get.connection.close() # Is it really needed?, I'm not sure.
+
+w = WhatIsWithStatement('https://www.google.com')
+s = ""
+
+with w:
+    s = w.request_get.json()
+
+print(s)
