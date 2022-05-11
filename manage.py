1f3bb7f08baf9c5994ce8a56af6ccfc424284863
Beomsoo Kim <bluewhale8202@gmail.com>
2022-05-11 13:06:02 +0900

    add I didn't study

--- manage.py
+++ manage.py
@@ -16,33 +16,33 @@ def touch(cur, top, participants):
 
 
 if __name__ == '__main__':
-    touch('4', 'bitcoin', PARTICIPANTS)
-    touch('8', 'cookbook', PARTICIPANTS)
+    touch('', 'bitcoin', PARTICIPANTS)
+    touch('', 'cookbook', PARTICIPANTS)
 
 
-def get_all_files(dname):
-    for name in os.listdir(dname):
-        path = os.path.join(dname, name)
-        if os.path.isdir(path):
-            yield from get_all_files(path)
-        else:
-            yield path
+# def get_all_files(dname):
+#     for name in os.listdir(dname):
+#         path = os.path.join(dname, name)
+#         if os.path.isdir(path):
+#             yield from get_all_files(path)
+#         else:
+#             yield path
 
-def migrate(root):
-    for path in list(get_all_files(root)):
-        for p in PARTICIPANTS:
-            if p + '/' in path:
-                break
-        else:
-            print('skip', path)
-            continue
+# def migrate(root):
+#     for path in list(get_all_files(root)):
+#         for p in PARTICIPANTS:
+#             if p + '/' in path:
+#                 break
+#         else:
+#             print('skip', path)
+#             continue
 
-        new_path = path.replace('/', '_')
-        new_former, chap, name, new_latter = new_path.split('_', 3)
-        new_path = new_former + f'/{p}/{chap}_{new_latter}'
+#         new_path = path.replace('/', '_')
+#         new_former, chap, name, new_latter = new_path.split('_', 3)
+#         new_path = new_former + f'/{p}/{chap}_{new_latter}'
 
-        print(path, new_path)
-        shutil.move(path, new_path)
+#         print(path, new_path)
+#         shutil.move(path, new_path)
 
 
 # if __name__ == '__main__':
